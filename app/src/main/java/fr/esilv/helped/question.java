aac439ac4c62adcd9ced94f9c2491ba5abb0b519
Carole DUMOULIN <carole.dumoulin12@gmail.com>
2020-03-24 15:18:53 +0100

    update: 24/03

---
+++ app/src/main/java/fr/esilv/helped/question.java
@@ -0,0 +1,10 @@
+package fr.esilv.helped;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class question extends AppCompatActivity {
+}
